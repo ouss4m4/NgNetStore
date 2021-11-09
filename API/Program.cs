e36f799bfe6f75c1b04b8454062d614ddb4843b9
oussama <bz.oussama@gmail.com>
2021-11-09 16:59:16 +0100

    seed data works

--- API/Program.cs
+++ API/Program.cs
@@ -23,6 +23,7 @@ using (var scope = app.Services.CreateScope())
     {
         var context = services.GetRequiredService<StoreContext>();
         await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
     }
     catch (Exception ex)
     {
