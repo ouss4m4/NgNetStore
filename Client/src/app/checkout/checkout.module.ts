a18a8453c83660979a04a2e3aa575038b01773e6
oussama <bz.oussama@gmail.com>
2021-11-16 15:47:31 +0100

    checkout module added

---
+++ Client/src/app/checkout/checkout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './checkout.component';
+
+
+
+@NgModule({
+  declarations: [
+    CheckoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CheckoutModule { }
